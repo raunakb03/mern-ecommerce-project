b648e7eba0c78c1959fd29015e5b949eaa1b99f3
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2022-12-23 01:00:08 +0530

    adding authentication to create, update and delete product so only admin can do these operations

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct } = require('../controllers/productController');
-const { update } = require('../models/productModel');
+const { isAdmin, authMiddleware } = require('../middlewares/authMiddleware')
 const router = express.Router();
 
-router.post('/', createProduct)
+router.post('/', authMiddleware, isAdmin, createProduct)
 router.get('/:id', getaProduct)
-router.put('/:id', updateProduct)
-router.delete('/:id', deleteProduct)
+router.put('/:id', authMiddleware, isAdmin, updateProduct)
+router.delete('/:id', authMiddleware, isAdmin, deleteProduct)
 router.get('/', getAllProduct)
 
 module.exports = router;
