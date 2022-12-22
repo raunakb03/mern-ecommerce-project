6f77eba17f77c9b64b557b7f1051db402c14a31e
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2022-12-23 00:44:47 +0530

    update and delete product

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -42,8 +42,33 @@ const getAllProduct = asyncHandler(async (req, res) => {
   }
 })
 
-// !@Function:   get all products
-// !@Method:     GET
-// !@Route:      api/product
+// !@Function:   update products
+// !@Method:     PUT
+// !@Route:      api/product/:id
+const updateProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+    const updateProduct = await Product.findOneAndUpdate({ id }, req.body, { new: true });
+    res.json(updateProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+})
 
-module.exports = { createProduct, getaProduct, getAllProduct };
+// !@Function:   delete products
+// !@Method:     DELETE
+// !@Route:      api/product/:id
+const deleteProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteProduct = await Product.findOneAndDelete(id);
+    res.json(deleteProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+})
+
+module.exports = { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct };
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { createProduct, getaProduct, getAllProduct } = require('../controllers/productController');
+const { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct } = require('../controllers/productController');
+const { update } = require('../models/productModel');
 const router = express.Router();
 
 router.post('/', createProduct)
 router.get('/:id', getaProduct)
+router.put('/:id', updateProduct)
+router.delete('/:id', deleteProduct)
 router.get('/', getAllProduct)
 
 module.exports = router;
