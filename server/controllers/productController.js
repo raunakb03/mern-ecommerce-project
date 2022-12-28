9daace0487d97bc38004f01e80c52ec1766df415
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2022-12-28 23:57:56 +0530

    sorting, limiting and pagination of products

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -45,15 +45,39 @@ const getAllProduct = asyncHandler(async (req, res) => {
 
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    
-    const query= Product.find(JSON.parse(queryStr));
 
-    const product= await query;
+    let query = Product.find(JSON.parse(queryStr));
+
+    // Sorting
+
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(" ");
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //Limiting the fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(" ");
+      query = query.select(fields);
+
+    } else {
+      query = query.select('-__v');
+    }
+
+    // Pagination
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const productCount = await Product.countDocuments();
+      if (skip >= productCount) throw new Error("THis page does not exists");
+    }
+
+    const product = await query;
     res.json(product);
-
-
-    const getAllProducts = await Product.find(queryObj);
-    res.json(getAllProducts);
   } catch (error) {
     throw new Error(error);
   }
