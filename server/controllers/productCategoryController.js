a59febf49a0fb24435f20c89ccc6b8b53fd2284d
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-01-03 19:44:47 +0530

    create delete category, get a category and get all category

--- server/controllers/productCategoryController.js
+++ server/controllers/productCategoryController.js
@@ -8,7 +8,7 @@ const validateMongodbId = require("../utils/validateMongodbId");
 // !@Route:       /api/category
 const createCategory = asynchHandler(async (req, res) => {
     try {
-        const newCategory= await Category.create(req.body);
+        const newCategory = await Category.create(req.body);
         res.json(newCategory);
     } catch (error) {
         throw new Error(error);
@@ -18,13 +18,55 @@ const createCategory = asynchHandler(async (req, res) => {
 // !@Function:    update a category
 // !@Method:      PUT
 // !@Route:       /api/category/:id
-const updateCategory = asynchHandler(async (req, res) =>{
-    const {id}= req.params;
+const updateCategory = asynchHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongodbId(id);
     try {
-        const updatedCategory= await Category.findByIdAndUpdate(id, req.body, {new: true});
+        const updatedCategory = await Category.findByIdAndUpdate(id, req.body, { new: true });
         res.json(updatedCategory);
     } catch (error) {
         throw new Error(error);
     }
 });
-module.exports = {createCategory, updateCategory};
+
+// !@Function:    delete a category
+// !@Method:      DELETE
+// !@Route:       /api/category/:id
+const deleteCategory = asynchHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongodbId(id);
+    try {
+        const deletedCategory = await Category.findByIdAndDelete(id);
+        res.json(deletedCategory);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+// !@Function:    get a category
+// !@Method:      GET
+// !@Route:       /api/category/:id
+const getCategory = asynchHandler(async (req, res) => {
+    const { id } = req.params;
+    validateMongodbId(id);
+    try {
+        const getaCategory = await Category.findById(id);
+        res.json(getaCategory);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+
+// !@Function:    get all category
+// !@Method:      GET
+// !@Route:       /api/category/
+const getAllCategory = asynchHandler(async (req, res) => {
+
+    try {
+        const getAllCategory = await Category.find();
+        res.json(getAllCategory);
+    } catch (error) {
+        throw new Error(error);
+    }
+});
+module.exports = { createCategory, updateCategory, deleteCategory, getCategory, getAllCategory };
